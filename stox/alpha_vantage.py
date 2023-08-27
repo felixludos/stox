566cb6997228b2643dab905a1c26a62cf9546e48
felixludos <felixludos.info@gmail.com>
2023-08-27 17:29:34 +0200

    minor cleanup

--- stox/alpha_vantage.py
+++ stox/alpha_vantage.py
@@ -2,8 +2,8 @@ from . import misc
 import requests
 
 
-class AlphaVantage:
-	_base_url = 'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={apikey}'
+class _AlphaVantageBase:
+	_query_url_template = 'https://www.alphavantage.co/query?{query}'
 
 	def __init__(self, API_KEY=None):
 		if API_KEY is None:
@@ -11,39 +11,45 @@ class AlphaVantage:
 		self._api_key = API_KEY
 
 
-	def _send_call(self, function, symbol):
-		url = self._base_url.format(function=function, symbol=symbol, apikey=self._api_key)
+	def _send_call(self, **params):
+		if 'apikey' not in params:
+			params['apikey'] = self._api_key
+		query = '&'.join(f'{key}={value}' for key, value in params.items())
+
+		url = self._query_url_template.format(query=query)
 		response = requests.get(url)
 		data = response.json()
 		return data
 
 
+
+class AlphaVantageStocks(_AlphaVantageBase):
 	def overview(self, symbol: str):
-		return self._send_call('OVERVIEW', symbol)
+		return self._send_call(function='OVERVIEW', symbol=symbol)
 
 	def income_statement(self, symbol: str):
-		return self._send_call('INCOME_STATEMENT', symbol)
+		return self._send_call(function='INCOME_STATEMENT', symbol=symbol)
 
 	def balance_sheet(self, symbol: str):
-		return self._send_call('BALANCE_SHEET', symbol)
+		return self._send_call(function='BALANCE_SHEET', symbol=symbol)
 
 	def cash_flow(self, symbol: str):
-		return self._send_call('CASH_FLOW', symbol)
+		return self._send_call(function='CASH_FLOW', symbol=symbol)
 
 	def earnings(self, symbol: str):
-		return self._send_call('EARNINGS', symbol)
+		return self._send_call(function='EARNINGS', symbol=symbol)
 
 	def earnings_calendar(self, symbol: str):
-		return self._send_call('EARNINGS_CALENDAR', symbol)
+		return self._send_call(function='EARNINGS_CALENDAR', symbol=symbol)
 
 	def ipo_calendar(self, symbol: str):
-		return self._send_call('IPO_CALENDAR', symbol)
+		return self._send_call(function='IPO_CALENDAR', symbol=symbol)
 
 	def listing_status(self, symbol: str):
-		return self._send_call('LISTING_STATUS', symbol)
+		return self._send_call(function='LISTING_STATUS', symbol=symbol)
 
 
-
+# add economic indicators, forex, etc.
 
 
 
